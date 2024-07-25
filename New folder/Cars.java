f816ba240415befc66612723de4c486ebd029aaf
unknown <amoghaganesh2002@gmail.com>
2024-07-25 14:01:03 +0530

    java program

---
+++ New folder/Cars.java
@@ -0,0 +1,24 @@
+public class Cars {
+    public static void main(String[] args) {
+        String[] cars = new String[15];
+        cars[0] = "Tesla Model S";
+        cars[1] = "Ford Mustang";
+        cars[2] = "Chevrolet Camaro";
+        cars[3] = "BMW 3 Series";
+        cars[4] = "Audi A4";
+        cars[5] = "Mercedes-Benz C-Class";
+        cars[6] = "Jaguar XF";
+        cars[7] = "Lexus ES";
+        cars[8] = "Honda Accord";
+        cars[9] = "Toyota Camry";
+        cars[10] = "Nissan Altima";
+        cars[11] = "Hyundai Sonata";
+        cars[12] = "Kia Optima";
+        cars[13] = "Mazda6";
+        cars[14] = "Subaru Legacy";
+
+        for (int i = 0; i < cars.length; i++) {
+            System.out.println(cars[i]);
+        }
+    }
+}
