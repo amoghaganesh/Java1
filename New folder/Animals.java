67220ce4a600cc22d1f99e38c89add1f3d54e706
unknown <amoghaganesh2002@gmail.com>
2024-07-25 13:54:10 +0530

    new program

---
+++ New folder/Animals.java
@@ -0,0 +1,29 @@
+public class Animals {
+    public static void main(String[] args) {
+        String[] animals = new String[20];
+        animals[0] = "Lion";
+        animals[1] = "Tiger";
+        animals[2] = "Elephant";
+        animals[3] = "Giraffe";
+        animals[4] = "Zebra";
+        animals[5] = "Monkey";
+        animals[6] = "Kangaroo";
+        animals[7] = "Panda";
+        animals[8] = "Penguin";
+        animals[9] = "Koala";
+        animals[10] = "Rhinoceros";
+        animals[11] = "Hippopotamus";
+        animals[12] = "Crocodile";
+        animals[13] = "Alligator";
+        animals[14] = "Eagle";
+        animals[15] = "Hawk";
+        animals[16] = "Owl";
+        animals[17] = "Wolf";
+        animals[18] = "Fox";
+        animals[19] = "Bear";
+
+        for (int i = 0; i < animals.length; i++) {
+            System.out.println(animals[i]);
+        }
+    }
+}
